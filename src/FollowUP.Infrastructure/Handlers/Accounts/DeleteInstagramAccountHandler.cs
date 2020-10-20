3211f23f77fcd97e293e4b9230559eff019f6659
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-10-20 16:42:44 +0200

    Add deleting InstagramAccount and Promotion handlers

--- src/FollowUP.Infrastructure/Handlers/Accounts/DeleteInstagramAccountHandler.cs
+++ src/FollowUP.Infrastructure/Handlers/Accounts/DeleteInstagramAccountHandler.cs
@@ -1,7 +1,10 @@
 ﻿using FollowUP.Core.Repositories;
 using FollowUP.Infrastructure.Commands;
 using FollowUP.Infrastructure.Exceptions;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FollowUP.Infrastructure.Handlers.Accounts
@@ -9,22 +12,47 @@ namespace FollowUP.Infrastructure.Handlers.Accounts
     public class DeleteInstagramAccountHandler : ICommandHandler<DeleteInstagramAccount>
     {
         private readonly IInstagramAccountRepository _instagramAccountRepository;
+        private readonly IProxyRepository _proxyRepository;
+        private readonly IScheduleRepository _scheduleRepository;
 
-        public DeleteInstagramAccountHandler(IInstagramAccountRepository instagramAccountRepository)
+        public DeleteInstagramAccountHandler(IInstagramAccountRepository instagramAccountRepository,
+            IProxyRepository proxyRepository, IScheduleRepository scheduleRepository)
         {
             _instagramAccountRepository = instagramAccountRepository;
+            _proxyRepository = proxyRepository;
+            _scheduleRepository = scheduleRepository;
         }
 
         public async Task HandleAsync(DeleteInstagramAccount command)
         {
-            var account = await _instagramAccountRepository.GetAsync(command.Username);
+            var account = await _instagramAccountRepository.GetAsync(command.InstagramAccountId);
 
             if (account == null)
+            {
                 return;
+            }
 
-            if (account.UserId != command.UserId)
+            if (account.User.Id != command.UserId)
+            {
                 throw new ServiceException(ErrorCodes.UserNotPermitted,
                     "Cannot delete account that doesn't belong to the current user.");
+            }
+
+            // Set instagram proxy free
+            if (account.InstagramProxy != null)
+            {
+                var proxy = await _proxyRepository.GetAsync(account.InstagramProxy.Id);
+                proxy.SetIsTaken(false);
+                await _proxyRepository.UpdateAsync(proxy);
+            }
+            
+            // Delete ScheduleGroups
+            IEnumerable<Guid> scheduleGroupsIds = account.ScheduleGroups?.Select(x => x.Id);
+            await _scheduleRepository.RemoveMultipleScheduleGroupsAsync(scheduleGroupsIds);
+
+            // Delete SingleScheduleDays
+            IEnumerable<Guid> singleScheduleDaysIds = account.SingleScheduleDays?.Select(x => x.Id);
+            await _scheduleRepository.RemoveMultipleSingleScheduleDaysAsync(singleScheduleDaysIds);
 
             // Delete account file if exists
             if (File.Exists(account.FilePath))
---
+++ src/FollowUP.Infrastructure/Handlers/Promotions/DeletePromotionHandler.cs
@@ -0,0 +1,41 @@
+﻿using FollowUP.Core.Repositories;
+using FollowUP.Infrastructure.Commands;
+using FollowUP.Infrastructure.Exceptions;
+using FollowUP.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace FollowUP.Infrastructure.Handlers.Accounts
+{
+    public class DeletePromotionHandler : ICommandHandler<DeletePromotion>
+    {
+        private readonly IPromotionRepository _promotionRepository;
+        private readonly IPromotionService _promotionService;
+
+        public DeletePromotionHandler(IPromotionRepository promotionRepository,
+            IPromotionService promotionService)
+        {
+            _promotionRepository = promotionRepository;
+            _promotionService = promotionService;
+        }
+
+        public async Task HandleAsync(DeletePromotion command)
+        {
+            var promotion = await _promotionRepository.GetAsync(command.PromotionId);
+
+            if (promotion == null)
+            {
+                throw new ServiceException(ErrorCodes.PromotionNotFound,
+                    $"Promotion with ID: {command.PromotionId} doesn't exist.");
+            }
+
+            if (promotion.InstagramAccount.User.Id != command.UserId)
+            {
+                throw new ServiceException(ErrorCodes.UserNotPermitted,
+                    $"Promotion with ID: {command.PromotionId} doesn't belong to any " +
+                    $"account that the user with ID: {command.UserId} owns.");
+            }
+
+            await _promotionService.DeletePromotionAsync(command.PromotionId);
+        }
+    }
+}
