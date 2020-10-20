b990b8be8b01e48c8f6746625bf1ef9a79442aca
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-10-20 16:46:17 +0200

    Add specialized Promotion mapping

---
+++ src/FollowUP.Infrastructure/Extensions/MapperExtenstions.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FollowUP.Core.Domain;
+using FollowUP.Infrastructure.DTO;
+using System.Collections.Generic;
+
+namespace FollowUP.Infrastructure.Extensions
+{
+    public static class MapperExtenstions
+    {
+        public static IEnumerable<PromotionDto> MapFollowPromotions(this IMapper mapper,
+            IEnumerable<FollowPromotion> promotions)
+        {
+            var promotionDtos = new List<PromotionDto>();
+
+            foreach (var promotion in promotions)
+            {
+                var promotionDto = mapper.Map<PromotionDto>(promotion);
+
+                promotionDto.PromotionType = promotion.GetPromotionType();
+
+                promotionDtos.Add(promotionDto);
+            }
+
+            return promotionDtos;
+        }
+    }
+}
