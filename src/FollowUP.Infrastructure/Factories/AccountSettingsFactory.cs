aa18dba7c673857005a3a8567917bf81fe9319cf
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-10-20 16:24:30 +0200

    Add AccountSettings factory

---
+++ src/FollowUP.Infrastructure/Factories/AccountSettingsFactory.cs
@@ -0,0 +1,13 @@
+﻿using FollowUP.Core.Domain;
+using System;
+
+namespace FollowUP.Infrastructure.Factories
+{
+    public static class AccountSettingsFactory
+    {
+        public static AccountSettings GetDefaultAccountSettings(Guid accountId)
+        {
+            return new AccountSettings(Guid.NewGuid(), accountId);
+        }
+    }
+}
