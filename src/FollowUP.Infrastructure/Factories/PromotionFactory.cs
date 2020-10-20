a966056ab720b2ce0ed9814e795b0d03d7ca4628
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-10-20 16:24:42 +0200

    Add Promotion factory

---
+++ src/FollowUP.Infrastructure/Factories/PromotionFactory.cs
@@ -0,0 +1,13 @@
+﻿using FollowUP.Core.Domain;
+using System;
+
+namespace FollowUP.Infrastructure.Factories
+{
+    public static class PromotionFactory
+    {
+        public static UnfollowPromotion GetUnfollowPromotion(Guid accountId)
+        {
+            return new UnfollowPromotion(Guid.NewGuid(), accountId, DateTime.Now);
+        }
+    }
+}
