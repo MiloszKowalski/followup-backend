f20b7d1e9bd0e8ea2a8038dea72469e88e3425d4
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-10-20 16:20:07 +0200

    Add Base64 string extensions

--- src/FollowUP.Infrastructure/Extensions/StringExtensions.cs
+++ src/FollowUP.Infrastructure/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace FollowUP.Infrastructure.Extensions
+﻿using System;
+using System.Text;
+
+namespace FollowUP.Infrastructure.Extensions
 {
     public static class StringExtensions
     {
@@ -8,8 +11,21 @@
         public static string Truncate(this string value, int length)
         {
             if (value.Length < length)
+            {
                 return value;
+            }
+
             return value.Substring(0, length);
+        }
+
+        public static string EncodeToBase64String(this string decoded)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(decoded));
+        }
+
+        public static string DecodeFromBase64String(this string encoded)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(encoded));
         }
     }
 }
